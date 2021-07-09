9718c42960ae039f4cdf5b619234dbdfe8548e09
Rojas Rafael <rafael.rojas@unibz.it>
2021-07-09 22:09:34 +0200

    [Functions] Print feature to functions added

--- tests/elemental_functions.cpp
+++ tests/elemental_functions.cpp
@@ -10,31 +10,96 @@ int main() {
   Sin sin(base_domain);
   Cos cos(base_domain);
 
-  FunctionExpression f_nom = (exp + sin + cos);
+  {
+    FunctionExpression f_nom = (exp + sin + cos);
 
-  FunctionExpression f_d_nom = (exp + cos - sin);
+    FunctionExpression f_d_nom = (exp + cos - sin);
 
-  for (int _ = 0; _ < 100; _++) {
-    Eigen::VectorXd time_span = Eigen::VectorXd::Random(5);
+    for (int _ = 0; _ < 100; _++) {
+      Eigen::VectorXd time_span = Eigen::VectorXd::Random(5);
 
-    assert((exp(time_span) + sin(time_span) + cos(time_span) - f_nom(time_span))
-               .norm() < 1.0e-9);
+      assert(
+          (exp(time_span) + sin(time_span) + cos(time_span) - f_nom(time_span))
+              .norm() < 1.0e-9);
 
-    FunctionExpression f_dot = f_nom.derivate();
+      FunctionExpression f_dot = f_nom.derivate();
 
-    f_dot.print();
+      f_dot.print();
 
-    printf("function name %s\n", f_dot.get_name().c_str());
+      assert(
+          (f_dot(time_span) - exp(time_span) - cos(time_span) + sin(time_span))
+              .norm() < 1.0e-9);
+    }
+  }
 
-    std::cout << f_dot(time_span) << "\n";
-    std::cout << exp(time_span) + cos(time_span) - sin(time_span) << "\n"
-              << (f_dot(time_span) - exp(time_span) + cos(time_span) -
-                  sin(time_span))
-                     .norm()
-              << "\n";
+  {
+    FunctionExpression f_nom = (exp * sin);
 
-    assert((f_dot(time_span) - exp(time_span) - cos(time_span) + sin(time_span))
-               .norm() < 1.0e-9);
+    FunctionExpression f_d_nom = (exp * cos) + (exp * sin);
+
+    for (int _ = 0; _ < 100; _++) {
+      Eigen::VectorXd time_span = Eigen::VectorXd::Random(5);
+
+      assert(((exp(time_span).array() * sin(time_span).array()).matrix() -
+              f_nom(time_span))
+                 .norm() < 1.0e-9);
+
+      FunctionExpression f_dot = f_nom.derivate();
+
+      f_dot.print();
+      std::cout << (f_dot(time_span) - f_d_nom(time_span)).norm() << "<< \n";
+      assert((f_dot(time_span) - f_d_nom(time_span)).norm() < 1.0e-9);
+      printf("------------ assertion paased\n\n");
+      fflush(stdout);
+    }
+  }
+
+  {
+    FunctionExpression f_nom = (exp + sin + cos);
+
+    FunctionExpression f_d_nom = (exp + cos - sin);
+
+    for (int _ = 0; _ < 100; _++) {
+      Eigen::VectorXd time_span = Eigen::VectorXd::Random(5);
+
+      assert(((exp(time_span) + sin(time_span) + cos(time_span)).matrix() -
+              f_nom(time_span))
+                 .norm() < 1.0e-9);
+
+      FunctionExpression f_dot = f_nom.derivate();
+
+      f_dot.print();
+
+      std::cout << (f_dot(time_span) - f_d_nom(time_span)).norm() << "<< \n";
+      fflush(stdout);
+      assert((f_dot(time_span) - f_d_nom(time_span)).norm() < 1.0e-9);
+    }
+  }
+
+  {
+    FunctionExpression f_nom = (exp + sin + cos) * cos;
+
+    FunctionExpression f_d_nom =
+        (exp + cos - sin) * cos + (exp + sin + cos) * (-sin);
+
+    for (int _ = 0; _ < 100; _++) {
+      Eigen::VectorXd time_span = Eigen::VectorXd::Random(5);
+
+      assert((((exp(time_span) + sin(time_span) + cos(time_span)).array() *
+               cos(time_span).array())
+                  .matrix() -
+              f_nom(time_span))
+                 .norm() < 1.0e-9);
+      printf("--------------------iiiiiiiiiiiiiiiiiiiii\n");
+
+      FunctionExpression f_dot = f_nom.derivate();
+
+      f_dot.print();
+
+      std::cout << (f_dot(time_span) - f_d_nom(time_span)).norm() << "<< \n";
+      fflush(stdout);
+      assert((f_dot(time_span) - f_d_nom(time_span)).norm() < 1.0e-9);
+    }
   }
 
   return 0;
