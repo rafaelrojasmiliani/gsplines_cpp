2859b14724c0b2173abe5ee19acd2a1426dccc4e
Rojas Rafael <rafael.rojas@unibz.it>
2021-09-20 17:34:50 +0200

    enable ctest

---
+++ CTestConfig.cmake
@@ -0,0 +1,7 @@
+set(CTEST_PROJECT_NAME "gsplines")
+find_program(MEMORYCHECK_COMMAND dsfsdf)
+set(MEMORYCHECK_COMMAND_OPTIONS
+    "--trace-children=yes --leak-check=full --error-exitcode=1 --show-leak-kinds=all --track-origins=yes --dsymutil=yes"
+)
+set(CTEST_MEMORYCHECK_SUPPRESSIONS_FILE
+    "${PROJECT_SOURCE_DIR}/valgrind-python.supp")
