eb709f823d0f9cf98e18474e4dbef18aaa4f40b6
Rafael Rojas <rafaelrojasmiliani@gmail.com>
2022-03-25 15:46:47 +0100

    update docker

--- docker/container-start.bash
+++ docker/container-start.bash
@@ -1,3 +1,4 @@
+
 main(){
 
     scriptdir=$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)
@@ -6,15 +7,33 @@ main(){
       exit 1
     fi
 
-    docker run -it \
-        --env="DISPLAY" \
-        --env="ROS_MASTER_URI" \
-        --env="ROS_MASTER_IP" \
-        --env="QT_X11_NO_MITSHM=1" \
-        --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-        --volume $(pwd)/../:/gsplinespp \
-        --user $(id -u):$(id -g) \
-        "gsplinespp" bash
+    if lspci | grep -qi "vga .*nvidia" && \
+        docker -D info 2>/dev/null | grep -qi "runtimes.* nvidia"; then
+        DOCKER_NVIDIA_OPTIONS="\
+            --runtime=nvidia \
+            --env=NVIDIA_VISIBLE_DEVICES=${NVIDIA_VISIBLE_DEVICES:-all} \
+            --env=NVIDIA_DRIVER_CAPABILITIES=${NVIDIA_DRIVER_CAPABILITIES:+$NVIDIA_DRIVER_CAPABILITIES,}all"
+    else
+        DOCKER_NVIDIA_OPTIONS=""
+    fi
+
+    XAUTH=/tmp/.docker.xauth
+    touch $XAUTH
+    xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+
+    DOCKER_VIDEO_OPTIONS="${DOCKER_NVIDIA_OPTIONS} --env=DISPLAY --env=QT_X11_NO_MITSHM=1 --env=XAUTHORITY=$XAUTH --volume=$XAUTH:$XAUTH --volume=/tmp/.X11-unix:/tmp/.X11-unix:rw"
+
+    myuid=$(id -u $USER)
+    mygid=$(id -g $USER)
+    mygroup=$(id -g -n $USER)
+    myuser="$USER"
+    docker pull rafa606/ros2-galactic-tutorial
+    docker run -it --rm \
+        ${DOCKER_VIDEO_OPTIONS} \
+        --volume $(pwd)/../:/workspace/src/tutorial: \
+        --entrypoint="/bin/bash" \
+        --privileged \
+        "rafa606/cpp-vim" -c "addgroup --gid ${mygid} ${mygroup} --force-badname;  adduser --gecos \"\" --disabled-password  --uid ${myuid} --gid ${mygid} ${myuser} --force-badname ; usermod -a -G video ${myuser}; echo ${myuser} ALL=\(ALL\) NOPASSWD:ALL >> /etc/sudoers; sudo -EHu ${myuser}  bash"
 }
 
 main
