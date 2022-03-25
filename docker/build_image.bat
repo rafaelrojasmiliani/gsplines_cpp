c70c0b04cfcab4628c99dd82bee685c943aed8f0
Rafael Rojas <rafaelrojasmiliani@gmail.com>
2022-03-25 18:17:46 +0100

    remove docker files

--- docker/build_image.bat
+++
@@ -1,9 +0,0 @@
-
-@ECHO OFF
-
-docker build -t "gsplinespp" ^
-    --build-arg myuser="%USERNAME%" ^
-    --build-arg myuid=11011 ^
-    --build-arg mygroup="%USERNAME%" ^
-    --build-arg mygid=11011 ^
-    --no-cache -f ./image.dockerfile .
