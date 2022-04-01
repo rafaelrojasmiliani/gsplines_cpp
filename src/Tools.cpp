138877f70ad57a483b490c07d35febb81522075c
Rafael Rojas <rafaelrojasmiliani@gmail.com>
2022-04-01 11:18:04 +0200

    improve matrix float comparisom

--- src/Tools.cpp
+++ src/Tools.cpp
@@ -16,6 +16,9 @@ bool approx_equal(double _lhs, double _rhs, double _tol) {
 bool approx_equal(const Eigen::MatrixXd &_lhs, const Eigen::MatrixXd &_rhs,
                   double _tol) {
 
+  if (_lhs.rows() != _rhs.rows() or _lhs.cols() != _rhs.cols())
+    return false;
+
   double err = (_lhs - _rhs).array().abs().maxCoeff();
   double lhs_max = (_lhs).array().abs().maxCoeff();
   double rhs_max = (_rhs).array().abs().maxCoeff();
