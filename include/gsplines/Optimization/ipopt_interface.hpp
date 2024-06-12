8a3ae27271a5037703538ab7aae6398fbab3b6fb
rafaelrojasmiliani <rafaelrojasmiliani@gmail.com>
2024-06-12 09:46:41 +0200

    improve code quality in ipopt_interface

--- include/gsplines/Optimization/ipopt_interface.hpp
+++ include/gsplines/Optimization/ipopt_interface.hpp
@@ -2,60 +2,59 @@
 
 #ifndef IPOPT_INTERFACE_H
 #define IPOPT_INTERFACE_H
-#include <cstddef>
 #include <eigen3/Eigen/Core>
 #include <gsplines/FunctionalAnalysis/Sobolev.hpp>
 #include <ifopt/constraint_set.h>
 #include <ifopt/cost_term.h>
 #include <ifopt/variable_set.h>
+#include <cstddef>
 
 namespace gsplines {
 namespace optimization {
 
 class TimeSegmentLenghtsVar : public ifopt::VariableSet {
-private:
+ private:
   double exec_time_;
   Eigen::VectorXd values_;
   ifopt::Component::Component::VecBound bounds_;
 
-public:
+ public:
   TimeSegmentLenghtsVar(std::size_t _num_intervals, double _exec_time);
-  virtual ~TimeSegmentLenghtsVar();
-  void SetVariables(const Eigen::VectorXd &_vec) override;
-  Eigen::VectorXd GetValues() const override;
-  ifopt::Component::VecBound GetBounds() const override;
+  ~TimeSegmentLenghtsVar() override = default;
+  void SetVariables(const Eigen::VectorXd& _vec) override;
+  [[nodiscard]] Eigen::VectorXd GetValues() const override;
+  [[nodiscard]] ifopt::Component::VecBound GetBounds() const override;
 };
 
 class ExecTimeConstraint : public ifopt::ConstraintSet {
-public:
+ public:
   ExecTimeConstraint(std::size_t _num_intervals, double _exec_time);
 
-  Eigen::VectorXd GetValues() const override;
-  ifopt::Component::VecBound GetBounds() const override;
+  [[nodiscard]] Eigen::VectorXd GetValues() const override;
+  [[nodiscard]] ifopt::Component::VecBound GetBounds() const override;
 
   void FillJacobianBlock(std::string _set_name,
-                         Jacobian &_jac_block) const override;
+                         Jacobian& _jac_block) const override;
 
-  virtual ~ExecTimeConstraint() = default;
+  ~ExecTimeConstraint() override = default;
 
-private:
+ private:
   Eigen::VectorXd values_;
-  double exec_time_;
   ifopt::Component::VecBound bounds_;
 };
 
 class SobolevNorm : public ifopt::CostTerm {
-public:
-  SobolevNorm(std::string _name,
-              const Eigen::Ref<const Eigen::MatrixXd> _waypoints,
-              const gsplines::basis::Basis &_basis,
-              std::vector<std::pair<std::size_t, double>> _weights);
+ public:
+  SobolevNorm(const std::string& _name,
+              const Eigen::Ref<const Eigen::MatrixXd>& _waypoints,
+              const gsplines::basis::Basis& _basis,
+              const std::vector<std::pair<std::size_t, double>>& _weights);
 
   double GetCost() const override;
-  void FillJacobianBlock(std::string var_set, Jacobian &jac) const override;
-  virtual ~SobolevNorm() = default;
+  void FillJacobianBlock(std::string var_set, Jacobian& jac) const override;
+  ~SobolevNorm() override = default;
 
-private:
+ private:
   /* data */
   std::unique_ptr<gsplines::basis::Basis> basis_;
   std::vector<std::pair<std::size_t, double>> weights_;
@@ -65,6 +64,6 @@ private:
   mutable Eigen::VectorXd buff_2_;
 };
 
-} // namespace optimization
-} // namespace gsplines
+}  // namespace optimization
+}  // namespace gsplines
 #endif /* IPOPT_INTERFACE_H */
--- src/Optimization/ipopt_interface.cpp
+++ src/Optimization/ipopt_interface.cpp
@@ -5,16 +5,17 @@ namespace optimization {
 
 TimeSegmentLenghtsVar::TimeSegmentLenghtsVar(std::size_t _num_intervals,
                                              double _exec_time)
-    : VariableSet(_num_intervals, "TimeSegmentLenghtsVar"),
-      exec_time_(_exec_time), values_(_num_intervals), bounds_(GetRows()) {
+    : VariableSet(static_cast<int>(_num_intervals), "TimeSegmentLenghtsVar"),
+      exec_time_(_exec_time),
+      values_(_num_intervals),
+      bounds_(GetRows()) {
   values_.setOnes();
   values_ *= exec_time_ / GetRows();
   ifopt::Bounds default_bound(0.0, ifopt::inf);
   bounds_ = ifopt::Component::VecBound(GetRows(), default_bound);
 }
-TimeSegmentLenghtsVar::~TimeSegmentLenghtsVar() {}
 
-void TimeSegmentLenghtsVar::SetVariables(const Eigen::VectorXd &_vec) {
+void TimeSegmentLenghtsVar::SetVariables(const Eigen::VectorXd& _vec) {
   values_ = _vec;
 }
 
@@ -27,13 +28,11 @@ ifopt::Component::VecBound TimeSegmentLenghtsVar::GetBounds() const {
 ExecTimeConstraint::ExecTimeConstraint(std::size_t _num_intervals,
                                        double _exec_time)
     : ConstraintSet(1, "ExecTimeConstraint"), values_(1) {
-
   ifopt::Bounds default_bound(_exec_time, _exec_time);
   bounds_ = ifopt::Component::VecBound(1, default_bound);
 }
 
 Eigen::VectorXd ExecTimeConstraint::GetValues() const {
-
   Eigen::VectorXd result(1);
 
   result(0) =
@@ -45,30 +44,36 @@ ifopt::Component::VecBound ExecTimeConstraint::GetBounds() const {
 }
 
 void ExecTimeConstraint::FillJacobianBlock(std::string _set_name,
-                                           Jacobian &_jac_block) const {
+                                           Jacobian& _jac_block) const {
+  (void)_set_name;
   for (unsigned int i = 0;
        i < GetVariables()->GetComponent("TimeSegmentLenghtsVar")->GetRows();
-       i++)
+       i++) {
     _jac_block.coeffRef(0, i) = 1.0;
+  }
 }
 
-SobolevNorm::SobolevNorm(std::string _name,
-                         const Eigen::Ref<const Eigen::MatrixXd> _waypoints,
-                         const gsplines::basis::Basis &_basis,
-                         std::vector<std::pair<std::size_t, double>> _weights)
-    : CostTerm(_name), basis_(_basis.clone()), weights_(_weights),
-      waypoints_(_waypoints), sobol_norm_(_waypoints, _basis, _weights),
-      buff_1_(_waypoints.rows() - 1), buff_2_(_waypoints.rows() - 1) {}
+SobolevNorm::SobolevNorm(
+    const std::string& _name,
+    const Eigen::Ref<const Eigen::MatrixXd>& _waypoints,
+    const gsplines::basis::Basis& _basis,
+    const std::vector<std::pair<std::size_t, double>>& _weights)
+    : CostTerm(_name),
+      basis_(_basis.clone()),
+      weights_(_weights),
+      waypoints_(_waypoints),
+      sobol_norm_(_waypoints, _basis, _weights),
+      buff_1_(_waypoints.rows() - 1),
+      buff_2_(_waypoints.rows() - 1) {}
 
 double SobolevNorm::GetCost() const {
-
   buff_1_.noalias() =
       GetVariables()->GetComponent("TimeSegmentLenghtsVar")->GetValues();
   return sobol_norm_(buff_1_);
 }
 void SobolevNorm::FillJacobianBlock(std::string _var_set,
-                                    Jacobian &_jac) const {
-
+                                    Jacobian& _jac) const {
+  (void)_var_set;
   buff_1_.noalias() =
       GetVariables()->GetComponent("TimeSegmentLenghtsVar")->GetValues();
 
@@ -76,8 +81,9 @@ void SobolevNorm::FillJacobianBlock(std::string _var_set,
 
   for (unsigned int i = 0;
        i < GetVariables()->GetComponent("TimeSegmentLenghtsVar")->GetRows();
-       i++)
+       i++) {
     _jac.coeffRef(0, i) = buff_2_(i);
+  }
 }
-} // namespace optimization
-} // namespace gsplines
+}  // namespace optimization
+}  // namespace gsplines
